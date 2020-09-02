dfb23e00dc563dc465c813765ca733e617c6873c
ankitkmistry <62993463+ankitkmistry@users.noreply.github.com>
2020-09-02 16:32:12 +0530

    Initial Commit

---
+++ src/drivers/driver.cpp
@@ -0,0 +1,20 @@
+#include <drivers/driver.h>
+using namespace myos::drivers;
+
+Driver::Driver()
+{
+}
+Driver::~Driver()
+{
+}
+
+void Driver::Activate()
+{
+}
+int Driver::Reset()
+{
+    return 0;
+}
+void Driver::Deactivate()
+{
+}
